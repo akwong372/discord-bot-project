803da2cbf3e383fff9201ebab3779d624cb9edb8
akwong372 <akwong372@gmail.com>
2018-06-21 21:07:36 -0700

    changed channel id

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const commando = require('discord.js-commando');
 const discord = require('discord.js');
-const bot = new commando.Client();
 const config = require("./config.json");
+const bot = new commando.Client({
+    commandPrefix: config.prefix
+});
 
 bot.registry.registerGroups([
     ['random', 'Random'],
@@ -12,7 +14,7 @@ bot.registry.registerCommandsIn(__dirname + '/commands');
 
 bot.on('ready', () => {
     console.log('Logged in!');
-    bot.user.setActivity('nico nico nii');
+    bot.user.setActivity('Use `help');
     let now = new Date();
     const annoyingArray = [`Wes-tan stop being a lolicon ☆⌒(*＾∇ﾟ)v ！`,
         `wessuuu~ stop touching little girls like me ˚‧º·(˚ ˃̣̣̥⌓˂̣̣̥ )‧º·˚`,
@@ -27,7 +29,7 @@ bot.on('ready', () => {
     setTimeout(function () {
         let rollAnnoy;
         rollAnnoy = Math.floor(Math.random() * annoyingArray.length);
-        bot.guilds.get(config.baeGeneralId).channels.get(config.baeId).send(annoyingArray[rollAnnoy]);
+        bot.guilds.get(config.maplesyrupGeneralId).channels.get(config.maplesyrupId).send(annoyingArray[rollAnnoy]);
     }, millisTill9);
 });
 
@@ -51,7 +53,7 @@ bot.on('message', (message) => {
                 message.channel.send(`P-please let me stay...`);
             }
         }
-    } else if (/bot-? ?chan,? you'?re (super)? ?(so)? ?(very)? ?(really)? ?(damn)? ?gross/i.test(message.toString())) {
+    } else if (/bot-? ?chan,? you'?re (super)? ?(so)? ?(very)? ?(really)? ?(damn)? ?(gross)?(gay)?(dumb)?(retarded)?/i.test(message.toString())) {
         message.channel.send(`:(`);
     } else if (/bot-? ?chan,? what do you think of rem[?]?/i.test(message.toString()) || /bot-? ?chan,? what do you think about rem[?]?/i.test(message.toString()) || /bot-? ?chan,? who'? ?i?s rem[?]?/i.test(message.toString())) {
         if (message.author.id == config.wesleyId) {
@@ -140,6 +142,34 @@ bot.on('message', (message) => {
             message.channel.send(`Hi ♪~`);
         }
 
+    } else if (/bot-? ?chan succ gu?(oo)?d[?]?/i.test(message.toString())) {
+        let rollSucc;
+        rollSucc = Math.floor(Math.random() * 2);
+        if (rollSucc == 0) {
+            message.channel.send(`U-umm... what?`);
+        } else if (rollSucc == 1) {
+            message.channel.send(`P-please don't be lewd`);
+        }
+    } else if (/bot-? ?chan,? who'? ?i?s ?(the)? ?worst?e? ?(gi?rl)? ?(waifu)?[?]?/i.test(message.toString())) {
+        let rollWorst;
+        rollWorst = Math.floor(Math.random() * 3);
+        if (rollWorst == 0) {
+            message.channel.send(`Not me~`);
+        } else if (rollWorst == 1) {
+            message.channel.send(`Re- All waifus are great!`);
+        } else if (rollWorst == 2) {
+            message.channel.send(`Akemi Homura`);
+        }
+    } else if (/bot-? ?chan,? who'? ?i?s ?(the)? ?best ?(gi?rl)? ?(waifu)?[?]?/i.test(message.toString())) {
+        let rollBest;
+        rollBest = Math.floor(Math.random() * 3);
+        if (rollBest == 0) {
+            message.channel.send(`Hatsune Miku`);
+        } else if (rollBest == 1) {
+            message.channel.send(`Rem, I guess`);
+        } else if (rollBest == 2) {
+            message.channel.send(`初音ミク`);
+        }
     }
 });
 
@@ -147,13 +177,13 @@ bot.on('message', (message) => {
 //     let newUserChannel = newMember.voiceChannel;
 //     let oldUserChannel = oldMember.voiceChannel;
 
-//     if (oldUserChannel === undefined && newUserChannel !== undefined && bot.guilds.get(config.baeId) !== undefined) {
+//     if (oldUserChannel === undefined && newUserChannel !== undefined && bot.guilds.get(config.maplesyrupId) !== undefined) {
 //         // User Joins a voice channel;
-//         bot.guilds.get(config.baeId).channels.get(config.baeGeneralId).send(`${newMember.user.username} has joined ${newUserChannel.name}! Hi~`);
+//         bot.guilds.get(config.maplesyrupId).channels.get(config.maplesyrupGeneralId).send(`${newMember.user.username} has joined ${newUserChannel.name}! Hi~`);
 
-//     } else if (newUserChannel === undefined && bot.guilds.get(config.baeId) !== undefined) {
+//     } else if (newUserChannel === undefined && bot.guilds.get(config.maplesyrupId) !== undefined) {
 //         // User leaves a voice channel
-//         bot.guilds.get(config.baeId).channels.get(config.baeGeneralId).send(`${newMember.user.username} has left ${oldUserChannel.name}! Bye~`);
+//         bot.guilds.get(config.maplesyrupId).channels.get(config.maplesyrupGeneralId).send(`${newMember.user.username} has left ${oldUserChannel.name}! Bye~`);
 //     }
 //})
 
